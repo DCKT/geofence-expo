c68727dac71d70aadb2b0bb00e77b9fd54ad828e
Thomas Deconinck <thomas.deconinck@colisweb.com>
2021-04-21 16:45:22 +0200

    init

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
+
+TaskManager.defineTask("test", ({ data: { eventType, region }, error }) => {
+  if (error) {
+    // check `error.message` for more details.
+    return;
+  }
+  if (eventType === Location.LocationGeofencingEventType.Enter) {
+    console.log("You've entered region:", region);
+  } else if (eventType === Location.LocationGeofencingEventType.Exit) {
+    console.log("You've left region:", region);
+  }
+});
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      let bg = await Location.requestBackgroundPermissionsAsync();
+      if (bg.status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      await Location.startGeofencingAsync("test", [
+        {
+          identifier: "test-1",
+          latitude: -21.39742,
+          longitude: -154.39865,
+          radius: 200,
+        },
+      ]);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
@@ -14,8 +61,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
